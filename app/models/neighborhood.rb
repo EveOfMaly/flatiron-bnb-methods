0f467d09e423e55b2ed32e641b03e1820ab8fde7
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-03-04 11:31:00 -0500

    Jeff Reviewing

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,11 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  include ReservesModules
+
+
+  def neighbor_openenings(start_date, end_date)
+    openings(start_date, end_date)
+  end
+
 end
